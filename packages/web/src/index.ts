30a3e882372595a7581a3330a359d219d7232cb9
fazeng <fazeng@clife.cn>
2025-03-06 13:33:41 +0800

    feat: Web 监控

---
+++ packages/web/src/index.ts
@@ -0,0 +1,4 @@
+import { init, SDK_VERSION, SDK_NAME, log } from "zfleaves-monitor-browser";
+import { MonitorVue } from "zfleaves-monitor-vue";
+import { errorBoundaryReport } from "zfleaves-monitor-react";
+export { init, SDK_VERSION, SDK_NAME, MonitorVue, log, errorBoundaryReport };
